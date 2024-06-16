d6ecd209fc16b7c003a3717871ae5297ec98ca03
DarrenBright <ndouanladarren@gmail.com>
2024-06-16 17:02:40 +0100

    profile_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,178 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Restaurant',
+      home: ProfilePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ProfilePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ProfilePageState createState() => _ProfilePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ProfilePageState extends State<ProfilePage> {
+  bool isDarkMode = false;
 
-  void _incrementCounter() {
+  void _toggleDarkMode(bool value) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      isDarkMode = value;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Profile'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          UserProfileHeader(isDarkMode: isDarkMode),
+          Expanded(
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.favorite),
+                  title: Text('My Favorite'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                ListTile(
+                  leading: Icon(Icons.local_offer),
+                  title: Text('Special Offers'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                ListTile(
+                  leading: Icon(Icons.credit_card),
+                  title: Text('Payements Methods'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                // Ligne horizontale
+                Divider(
+                  color: isDarkMode ? Colors.grey[700] : Colors.grey[300],
+                  thickness: 1,
+                  indent: 16,
+                  endIndent: 16,
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Profile'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                ListTile(
+                  leading: Icon(Icons.location_on),
+                  title: Text('Address'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                ListTile(
+                  leading: Icon(Icons.notifications),
+                  title: Text('Notification'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                ListTile(
+                  leading: Icon(Icons.lock),
+                  title: Text('Security'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                ListTile(
+                  leading: Icon(Icons.language),
+                  title: Text('Language '),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                ListTile(
+                  leading: Icon(Icons.nightlight_round),
+                  title: Text('Dark Mode'),
+                  trailing: Switch(
+                    value: isDarkMode,
+                    onChanged: _toggleDarkMode,
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.help),
+                  title: Text('Help'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                ListTile(
+                  leading: Icon(Icons.person_add),
+                  title: Text('Invite Friends'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.logout,
+                    color: isDarkMode ? Colors.red : null,
+                  ),
+                  title: Text('Logout'),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    color: isDarkMode ? Colors.red : null,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor, // Appliquer la couleur primaire à la barre de navigation
+        selectedItemColor: Colors.green, // Couleur des icônes sélectionnées
+        unselectedItemColor: isDarkMode ? Colors.grey[400] : Colors.grey, // Couleur des icônes non sélectionnées
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Orders'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Message'),
+          BottomNavigationBarItem(icon: Icon(Icons.credit_card), label: 'E-Wallet'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
+
+class UserProfileHeader extends StatelessWidget {
+  final bool isDarkMode;
+
+  UserProfileHeader({required this.isDarkMode});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/profile.jpg'),
+            radius: 30,
+          ),
+          SizedBox(width: 16.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Darren Bright',
+                style: TextStyle(
+                  color: isDarkMode ? Colors.white : Colors.black,
+                ),
+              ),
+              SizedBox(height: 4.0),
+              Text(
+                '+237 651630073',
+                style: TextStyle(
+                  color: isDarkMode ? Colors.grey[400] : Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
