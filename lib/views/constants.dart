263aa5a50bd9b04e74d41de77fda85c295424664
Juniorlk <juniorlekeu@gmail.com>
2024-06-19 14:34:12 +0100

    baseUrl

--- lib/views/constants.dart
+++ lib/views/constants.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 
-const String baseUrl = "http://192.168.1.197:80/api";
+const String baseUrl = "http://192.168.90.219:80/api";
 
 const primaryColor = Color(0xFF1BAC4B);
 const secondaryColor = Color(0xFF2A2D3E);
